9de0417c8b66244f7a5e05bc7eb01c324a7859cd
Danny Matute <dannyhmatute@outlook.es>
2023-06-03 21:42:43 -0600

    Initial repository

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  return (
+    <div data-aos="flip-left"
+    data-aos-easing="ease-out-cubic"
+    data-aos-duration="2000"> 
+    <h2>Este es mi segundo título</h2>
+    </div>
+  );
+}
